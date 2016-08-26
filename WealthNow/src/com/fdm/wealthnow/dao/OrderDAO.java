59f4451298233bc15c9a491334fde4be71076ffe
Saifullah.R <Saifullah.R@LAPBTN11975.fdmgroup.local>
2016-08-26 16:07:57 +0800

    ORDER DAO added

--- WealthNow/src/com/fdm/wealthnow/dao/OrderDAO.java
+++ WealthNow/src/com/fdm/wealthnow/dao/OrderDAO.java
@@ -1,33 +1,151 @@
 package com.fdm.wealthnow.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import com.fdm.wealthnow.common.Order;
 
-public class OrderDAO {
-	
-	public List getAllProcessedOrderFromUser(Integer user_id){
-	   return null;
+public class OrderDAO extends BaseDAO {
+
+	public List getAllProcessedOrderFromUser(Integer user_id) throws Exception {
+		List<Order> processedCompletedOrderfromUser = new ArrayList<Order>();
+
+		String SQL = "SELECT * FROM PROCESSEDORDER WHERE STATUS = 'completed' AND USER_ID = ?";
+		Connection connect = getConnection();
+		PreparedStatement ps = connect.prepareStatement(SQL);
+		ps.setInt(1, user_id);
+
+		ResultSet result = ps.executeQuery();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+
+		while (result.next()) {
+			Integer order_id = result.getInt("order_id");
+			String currency_code = result.getString("currency_code");
+			String order_type = result.getString("order_type");
+			Integer quantity = result.getInt("quantity");
+			String stock_symbol = result.getString("stock_symbol");
+			String price_type = result.getString("price_type");
+			Date opening_order_date = result.getDate("opening_order_date");
+			Float limit_price = result.getFloat("limit_price");
+			String term = result.getString("term");
+			String status = "OpenOrder";
+			Order order = new Order(user_id, order_id, currency_code, order_type, quantity, stock_symbol, price_type,
+					opening_order_date, limit_price, term, status);
+			processedCompletedOrderfromUser.add(order);
+		}
+
+		return processedCompletedOrderfromUser;
 	}
-	
-	public List getAllSoldOrderInDatabase(){
-	   return null;
+
+	public List getAllSoldOrderInDatabase() throws Exception {
+		List<Order> AllSoldOrderInDatabase = new ArrayList<Order>();
+
+		String SQL = "SELECT * FROM PROCESSEDORDER WHERE STATUS = 'completed'";
+		Connection connect = getConnection();
+		PreparedStatement ps = connect.prepareStatement(SQL);
+
+		ResultSet result = ps.executeQuery();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+
+		while (result.next()) {
+			Integer order_id = result.getInt("order_id");
+			Integer user_id = result.getInt("user_id");
+			String currency_code = result.getString("currency_code");
+			String order_type = result.getString("order_type");
+			Integer quantity = result.getInt("quantity");
+			String stock_symbol = result.getString("stock_symbol");
+			String price_type = result.getString("price_type");
+			Date opening_order_date = result.getDate("opening_order_date");
+			Float limit_price = result.getFloat("limit_price");
+			String term = result.getString("term");
+			String status = "OpenOrder";
+			Order order = new Order(user_id, order_id, currency_code, order_type, quantity, stock_symbol, price_type,
+					opening_order_date, limit_price, term, status);
+			AllSoldOrderInDatabase.add(order);
+		}
+
+		return AllSoldOrderInDatabase;
 	}
-	
-	public void createProcessedOrderInDatabase(){
+
+	public void createProcessedOrderInDatabase(Integer user_id, Integer order_id, String currency_code,
+			String order_type, Integer quantity, String stock_symbol, String price_type, Date opening_order_date,
+			Float limit_price, Date order_completion_date, String status, Float closing_price) throws Exception {
+
+		String SQL = "INSERT INTO PROCESSEDORDER (order_id, user_id, currency_code, "
+				+ "order_type, quantity, stock_symbol, "
+				+ "price_type, status ,place_order_date, limit_price, order_completion_date, closing_price) "
+				+ "VALUES( '" + user_id + "','" + order_id + "','" + currency_code + "','" + order_type + "',"
+				+ quantity + ",'" + stock_symbol + "','" + price_type + "','" + status + "','" + opening_order_date
+				+ "'," + limit_price + "','" + order_completion_date + "'," + limit_price + ")";
+
+		Connection connect = getConnection();
+		PreparedStatement ps = connect.prepareStatement(SQL);
+		ps.executeUpdate();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+
 	}
-	
-	public void updateProcessedOrderInDatabase(){
+
+	public List getListOfOpenOrder(Integer count) throws Exception {
+		List<Order> OpenOrderList = new ArrayList<Order>();
+
+		String SQL = "SELECT * FROM OPENORDER LIMIT ?";
+		Connection connect = getConnection();
+		PreparedStatement ps = connect.prepareStatement(SQL);
+		ps.setInt(1, count);
+
+		ResultSet result = ps.executeQuery();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+
+		while (result.next()) {
+			Integer order_id = result.getInt("order_id");
+			Integer user_id = result.getInt("user_id");
+			String currency_code = result.getString("currency_code");
+			String order_type = result.getString("order_type");
+			Integer quantity = result.getInt("quantity");
+			String stock_symbol = result.getString("stock_symbol");
+			String price_type = result.getString("price_type");
+			Date opening_order_date = result.getDate("opening_order_date");
+			Float limit_price = result.getFloat("limit_price");
+			String term = result.getString("term");
+			String status = "OpenOrder";
+			Order order = new Order(user_id, order_id, currency_code, order_type, quantity, stock_symbol, price_type,
+					opening_order_date, limit_price, term, status);
+			OpenOrderList.add(order);
+		}
+
+		return OpenOrderList;
 	}
-	
-	public List getListOfOpenOrder(Integer count){
-       return null;
+
+	public void createOpenOrderInDatabase(Integer user_id, Integer order_id, String currency_code, String order_type,
+			Integer quantity, String stock_symbol, String price_type, Date opening_order_date, Float limit_price,
+			String term, String status) throws Exception {
+
+		String SQL = "INSERT INTO OPENORDER (order_id, user_id, currency_code, "
+				+ "order_type, quantity, stock_symbol, " + "price_type, opening_order_date, limit_price, term) "
+				+ "VALUES( '" + user_id + "','" + order_id + "','" + currency_code + "','" + order_type + "',"
+				+ quantity + ",'" + stock_symbol + "','" + price_type + "','" + opening_order_date + "'," + limit_price
+				+ ",'" + term + ")";
+		Connection connect = getConnection();
+		PreparedStatement ps = connect.prepareStatement(SQL);
+		ps.executeUpdate();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+
 	}
-	
-	public void createOpenOrderInDatabase(){
+
+	public boolean deleteOpenOrderInDatabase(Integer order_id) throws Exception {
+		
+		Connection connect = getConnection();
+		connect.setAutoCommit(false);
+		String SQL = "DELETE FROM OPENORDER WHERE ORDER_ID =?";
+		PreparedStatement ps = connect.prepareStatement(SQL);
+		ps.setInt(1, order_id);
+		ps.executeUpdate();
+		connect.commit();
+		System.out.println("The SQL statement below has been executed\n" + SQL);
+		return true;
 	}
-	
-	public boolean deleteOpenOrderInDatabase(){
-       return true;
-	}
-	
+
 }
