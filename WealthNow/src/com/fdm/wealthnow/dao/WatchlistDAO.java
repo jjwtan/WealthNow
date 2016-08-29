c897e2714287e49021aaaa28d51d7960dc4ac796
Jing.Liou <liou.jing@u.nus.edu>
2016-08-29 16:58:26 +0800

    updated WatchlistDAO and WatchlistDAOTest

--- WealthNow/src/com/fdm/wealthnow/dao/WatchlistDAO.java
+++ WealthNow/src/com/fdm/wealthnow/dao/WatchlistDAO.java
@@ -1,63 +1,143 @@
 package com.fdm.wealthnow.dao;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.fdm.wealthnow.common.Watchlist;
+import com.fdm.wealthnow.util.DBUtil;
 
-public class WatchlistDAO extends BaseDAO {
-	
-	// test push
-	// return watchlist object 
-	public static Watchlist getWatchlist(int watchlistId){
-		
+import oracle.jdbc.proxy.annotation.Pre;
+
+public class WatchlistDAO extends DBUtil {
+
+	// return watchlist object
+	public static Watchlist getWatchlist(int watchlistId) {
+
 		Watchlist newWatchlist = new Watchlist();
-		
+
 		try {
-			PreparedStatement ps= getConnection().prepareStatement("select watchlist_name, visibility, date_created, date_modified from watchlist where watchlist_id = ?");
-			
-			//execute query 
-			ResultSet rs=ps.executeQuery();  
-			boolean status=rs.next();  
-			if (!status) {
-				// No row found for user name
-				// return failing reason 
+			Connection connect = getConnection();
+			// SQL statement
+			String SQLStatement = "select watchlist_name, visibility, date_created, date_modified from watchlist where watchlist_id = ?";
+			PreparedStatement ps = connect.prepareStatement(SQLStatement);
+			// insert the first ? as watchlistId
+			ps.setInt(1, watchlistId);
+
+			// execute query
+			ResultSet result = ps.executeQuery();
+			System.out.println("The SQL statement below has been executed\n" + SQLStatement);
+
+			while (result.next()) {
+				String watchlistName = result.getString("watchlist_name");
+				String visibility = result.getString("visibility");
+				Date dateCreated = result.getDate("date_created");
+				Date dateLastEdited = result.getDate("date_modified");
+				newWatchlist = new Watchlist(watchlistId, watchlistName, visibility, dateCreated, dateLastEdited);
 			}
 			
-			String watchlistName = rs.getString("watchlist_name");
-			String visibility = rs.getString("visibility");
-			Date dateCreated = rs.getDate("date_created");
-			Date dateLastEdited = rs.getDate("date_modified");
+			connect.close();
+			ps.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return newWatchlist;
+	};
+
+	// get all watchlists belonging to one user
+	public static List<Watchlist> getAllUserWatchlist(int userId) {
+		
+		System.out.println("Inside getAllUserWatchlist in WatchlistDAO");
+		List<Watchlist> watchlistsBelongingToUser = new ArrayList<Watchlist>();
+
+		try {
+			Connection connect = getConnection();
+			// SQL statement
+			String SQLStatement = "Select watchlist.watchlist_id, watchlist.watchlist_name, watchlist.visibility, watchlist.date_created, watchlist.date_modified from watchlist inner join userwatchlist on watchlist.watchlist_id = userwatchlist.watchlist_id inner join user1 on user1.user_id = userwatchlist.user_id where user1.user_id = ?";
+			PreparedStatement ps = connect.prepareStatement(SQLStatement);
+			// insert the first ? as watchlistId
+			ps.setInt(1, userId);
+
+			// execute query
+			ResultSet result = ps.executeQuery();
+			System.out.println("The SQL statement below has been executed\n" + SQLStatement);
+
+			while (result.next()) {
+				Integer watchlistId = result.getInt("watchlist_id");
+				String watchlistName = result.getString("watchlist_name");
+				String visibility = result.getString("visibility");
+				Date dateCreated = result.getDate("date_created");
+				Date dateLastEdited = result.getDate("date_modified");
+				Watchlist newWatchlist = new Watchlist(watchlistId, watchlistName, visibility, dateCreated, dateLastEdited);
+				watchlistsBelongingToUser.add(newWatchlist);		
+			}
 			
-			rs.close();
+			connect.close();
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return watchlistsBelongingToUser;
+	};
+
+	// return watchlist id
+	public static int addWatchlist(Watchlist watchlist, Integer userId) {
+		
+		System.out.println("Inside addWatchlist in WatchlistDAO");
+		Watchlist watchlistToAdd = new Watchlist();
+		
+		try {
+			Connection connect = getConnection();
 			
-			newWatchlist = new Watchlist(watchlistId, watchlistName, visibility, dateCreated, dateLastEdited);
+			// PART 1: insert record into watchlist 
+			String SQLStatement = "INSERT INTO Watchlist(watchlist_id, watchlist_name, visibility, date_created, date_modified) VALUES (?, ?, ?, ?, ?)";
+			PreparedStatement ps = connect.prepareStatement(SQLStatement);
+			
+			ps.setInt(1, watchlist.getWatchlistId());
+			ps.setString(2, watchlist.getWatchlistName());
+			ps.setString(3, watchlist.getVisibility());
+			ps.setDate(4, (Date) watchlist.getDateCreated());
+			ps.setDate(5, (Date) watchlist.getDateLastEdited());
+			
+			ps.executeUpdate();
+			System.out.println("The SQL statement below has been executed\n" + SQLStatement);
+			
+			// PART 2: insert userid to watchlistid into mapping table
+			String SQLStatement2 = "INSERT INTO UserWatchlist(user_id , watchlist_id) VALUES (?, ?)";
+			PreparedStatement ps2 = connect.prepareStatement(SQLStatement2);
+			
+			ps2.setInt(1, userId);
+			ps2.setInt(2, watchlist.getWatchlistId());
+			
+			ps2.executeUpdate(); 
+			System.out.println("The SQL statement below has been executed\n" + SQLStatement2);
+			
+			// PART 3: commit
+			String SQLStatement3 = "commit";
+			PreparedStatement ps3 = connect.prepareStatement(SQLStatement3);
+			ps3.executeQuery();
+			System.out.println("The SQL statement below has been executed\n" + SQLStatement3);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
-		return newWatchlist;
-	};
-	
-	//get all watchlists belonging to one user
-	public static List<Watchlist> getAllUserWatchlist(int userId){
-		return null;
-	};
-	
-	// return watchlist id 
-	public static int addWatchlist(Watchlist watchlist){
-		return 1;
-	};
-	
-	public static void deleteWatchlist(int watchListId){
-		
-	};
-	
-	public static boolean updateWatchlist(Watchlist watchlist){
-		return true;
+		}
+		return watchlist.getWatchlistId();
 	};
 
+	public static void deleteWatchlist(int watchListId) {
+
+	};
+
+	public static boolean updateWatchlist(Watchlist watchlist) {
+		return true;
+	};
 
 }
--- WealthNow/src/com/fdm/wealthnow/test/WatchlistDAOTest.java
+++ WealthNow/src/com/fdm/wealthnow/test/WatchlistDAOTest.java
@@ -2,8 +2,11 @@ package com.fdm.wealthnow.test;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.junit.Before;
@@ -13,47 +16,88 @@ import com.fdm.wealthnow.common.Watchlist;
 import com.fdm.wealthnow.dao.WatchlistDAO;
 import com.fdm.wealthnow.util.DatabaseConnectionFactory.ConnectionType;
 
+@SuppressWarnings("static-access")
 public class WatchlistDAOTest {
-	
-	private WatchlistDAO watchlistDAO;
-	
-	/*
-	static WatchlistDAO watchlistDAO = new WatchlistDAO();
-	static List<Watchlist> watchlists = new ArrayList<>();
-	
-	static {
-		
-		//int watchlistId, String watchlistName, String visibility, Date dateCreated, Date dateLastEdited
-		Date nowDate = new Date(); //current date 
-		watchlists.add(new Watchlist(1, "watchlist one", "private", nowDate, nowDate));
-		watchlists.add(new Watchlist(2, "watchlist two", "public", nowDate, nowDate));
-		watchlists.add(new Watchlist(3, "watchlist three", "private", nowDate, nowDate));
-	}
-	*/
 
-	@Before 
-	public void setup(){
+	private WatchlistDAO watchlistDAO;
+
+	@Before
+	public void setup() {
 		WatchlistDAO.setConnectionType(ConnectionType.LOCAL_CONNECTION);
 	}
-	
+
 	@Test
 	public void testGetWatchlist() {
-		/*
-				stockService = new StockService();
-		List<Stock> stocks = stockService.getStocksFromExchange(stockList);
-		for(Stock stock: stocks) {
-			System.out.println(stock);
-		}
-		 */
-		
+
+		System.out.println("Inside testGetWatchlist");
 		watchlistDAO = new WatchlistDAO();
-		//Watchlist watchlistToGet = new Watchlist();
-		@SuppressWarnings("static-access")
-		Watchlist watchlistToGet = watchlistDAO.getWatchlist(2);
-		
-		System.out.println(watchlistToGet);
-		
-		
+		// Watchlist watchlistToGet = new Watchlist();
+
+		Watchlist watchlistToGet1 = watchlistDAO.getWatchlist(1);
+		assertEquals("following", watchlistToGet1.getWatchlistName());
+		Watchlist watchlistToGet2 = watchlistDAO.getWatchlist(2);
+		assertEquals("myList", watchlistToGet2.getWatchlistName());
+		/*
+		 * Watchlist watchlistToGet3 = watchlistDAO.getWatchlist(4); // date
+		 * format shows like this assertEquals("2016-03-15",
+		 * watchlistToGet3.getDateCreated());
+		 */
 	}
 
+	@Test
+	public void testGetAllUserWatchlist() {
+
+		// set user_id and number of watchlists to get
+		// Integer userId = 3;
+		// Integer watchlistSize = 4;
+
+		System.out.println("Inside testGetAllUserWatchlist");
+		watchlistDAO = new WatchlistDAO();
+		List<Watchlist> watchlistsBelongingToUser = new ArrayList<Watchlist>();
+		watchlistsBelongingToUser = watchlistDAO.getAllUserWatchlist(3);
+
+		assertEquals(4, watchlistsBelongingToUser.size());
+
+		/*
+		 * for(int i=0; i<watchlistsBelongingToUser.size(); i++){
+		 * assertEquals("following", watchlistToGet1.getWatchlistName()); }
+		 */
+	}
+
+	@Test
+	public void testAddWatchlist() throws ParseException {
+		
+		System.out.println("--> Inside testAddWatchlist");
+		Integer userId;
+		Watchlist watchlistToAdd = new Watchlist();
+		
+		/* Solution online:
+DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+Date myDate = formatter.parse(date);
+java.sql.Date sqlDate = new java.sql.Date(myDate.getTime());
+		 */		
+		
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+		String dateString1 = "03-06-2015";
+		String dateString2 = "25-02-2016";
+		Date date1 = (Date) formatter.parse(dateString1);
+		Date date2 = (Date) formatter.parse(dateString2);
+		
+		java.sql.Date sqlDate1 = new java.sql.Date(date1.getTime());
+		java.sql.Date sqlDate2 = new java.sql.Date(date2.getTime());
+		userId = 2;
+
+		watchlistToAdd = new Watchlist(10, "Amys watchlist five", "PRIVATE", sqlDate1, sqlDate2);
+		watchlistDAO = new WatchlistDAO();
+		
+		// create watchlist for user
+		System.out.println("--> Calling addWatchlist for userId: " + userId);
+		watchlistDAO.addWatchlist(watchlistToAdd, userId);
+
+		// check that all the user's watchlists are present
+		List<Watchlist> watchlistsBelongingToUser = new ArrayList<Watchlist>();
+		watchlistsBelongingToUser = watchlistDAO.getAllUserWatchlist(2);
+
+		assertEquals(5, watchlistsBelongingToUser.size());
+	}
 }
