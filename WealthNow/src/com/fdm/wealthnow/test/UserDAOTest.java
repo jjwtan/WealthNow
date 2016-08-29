c6307544289b4968e35ca537bd98b6bd4376e727
Jeremy.Tan <Jeremy.Tan@LAPBTN12791.fdmgroup.local>
2016-08-29 15:37:40 +0800

    UserDAO test for single user

--- WealthNow/src/com/fdm/wealthnow/test/UserDAOTest.java
+++ WealthNow/src/com/fdm/wealthnow/test/UserDAOTest.java
@@ -2,13 +2,18 @@ package com.fdm.wealthnow.test;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import com.fdm.wealthnow.common.InfoType;
+import com.fdm.wealthnow.common.User;
 import com.fdm.wealthnow.dao.UserDAO;
 import com.fdm.wealthnow.util.DatabaseConnectionFactory.ConnectionType;
 
-public class UserTest {
+public class UserDAOTest {
+	static UserDAO userDAO = new UserDAO();
 
 	@Before
 	public void setUp() throws Exception {
@@ -16,8 +21,31 @@ public class UserTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUserTable() {
+		List<User> users = null;
+		try {
+			users = userDAO.getAllUser();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (User user : users) {
+			System.out.println(user.getUsername() + ": " + user);
+		}
+	}
+	
+	@Test
+	public void testSingleUser() {
+		try {
+			User user = userDAO.getUser(1, InfoType.BASIC);
+			
+			
+			System.out.println(user);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
