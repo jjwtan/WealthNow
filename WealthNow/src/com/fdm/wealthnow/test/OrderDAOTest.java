41ed7fc62577385f68832e703fab3f96832caf9a
Saifullah.R <Saifullah.R@LAPBTN11975.fdmgroup.local>
2016-08-29 16:05:16 +0800

    Tested all 8 methods in OrderDAO.

--- WealthNow/src/com/fdm/wealthnow/test/OrderDAOTest.java
+++ WealthNow/src/com/fdm/wealthnow/test/OrderDAOTest.java
@@ -7,40 +7,82 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import com.fdm.wealthnow.common.Order;
 import com.fdm.wealthnow.dao.OrderDAO;
+import com.fdm.wealthnow.util.DBUtil;
 import com.fdm.wealthnow.util.DatabaseConnectionFactory.ConnectionType;
 
-public class OrderDAOTest {
-	private Order order1, order2, order3, order4, order5;
+public class OrderDAOTest extends DBUtil {
 	private OrderDAO orderDAO;
+
+	@Before
+	public void setup() {
+		OrderDAO.setConnectionType(ConnectionType.LOCAL_CONNECTION);
+	}
+
+	@Test
+	public void testCreatedDataInDatabase() throws Exception {
+		orderDAO = new OrderDAO();
+		Integer order_id1 = getSequenceID("order_id_seq");
+		
+		orderDAO.createOpenOrderInDatabase(order_id1, new Integer(1), "SGD", "B", new Integer(100), "AAPL", "M", "11 Sep 2011",
+				new Double(88.9), "GC", "Open");
+		System.out.println("Start testCreatedDataInDatabase");
+		
+		Order order1 = orderDAO.getOrderFromOpenOrder(order_id1);
+		assertEquals(order1.getOrder_id(), order_id1);
+		
+		orderDAO.deleteOpenOrderInDatabase(order_id1);
+		System.out.println("Deleted "+order_id1+" successfully.");
+
+		
+		System.out.println("Test Completed: Created Data in database.");
+		
+	}
 	
-@Before
-public void setup(){
-	OrderDAO.setConnectionType(ConnectionType.LOCAL_CONNECTION);
-}
+	@Test
+	public void testGetListOfOpenOrder() throws Exception{
+		orderDAO = new OrderDAO();
+		List<Order> TestList = orderDAO.getListOfOpenOrder(3);
+		Integer testcount = TestList.size();
+		assertEquals(testcount, new Integer(3));
+	}
 
-@Test 
-public void testCreatedDataInDatabase() throws Exception{
-	orderDAO = new OrderDAO();
-	orderDAO.createOpenOrderInDatabase(new Integer(1), new Integer(100), "SGD", "B", new Integer(100), "AAPL", "M", "11 Sep 2011", new Double(88.9), "GC", "Open");
-	orderDAO.createOpenOrderInDatabase(new Integer(2), new Integer(101), "SGD", "B", new Integer(100), "GOOG", "LT", "05/05/2016", new Double(76.2), "GC", "Open");
-	orderDAO.createOpenOrderInDatabase(new Integer(3), new Integer(102), "SGD", "B", new Integer(100), "AMZN", "M", "06/05/2016", new Double(90.1), "GD", "Open");
-
-	List<Order> TestList = orderDAO.getListOfOpenOrder(3);
-	Integer testcount = TestList.size();
-
-	assertEquals(testcount, new Integer(3));
-	System.out.println("Test Completed: Created Data in database.");
-}
-
-
-@Test 
-public void testDeleteDataInOpenOrder() throws Exception{
-	orderDAO = new OrderDAO();
-	boolean deletedOrder = orderDAO.deleteOpenOrderInDatabase(80);
-	boolean isDeleted = true;
+	@Test
+	public void testCreateProcessedOrderData() throws Exception{
+		orderDAO = new OrderDAO();
+		
+		Integer order_id1 = getSequenceID("order_id_seq");
+		System.out.println("order id is " + order_id1);
+		orderDAO.createProcessedOrderInDatabase(order_id1, 1, "SGD", "B", 100, "SAISAI","M", "19 Sep 2014", 90.9, 
+				"12 Oct 2015", "cancelled", 90.2);
+		Order order = orderDAO.getOrderFromProcessedOrder(order_id1);
+		assertEquals(order.getUser_id(), new Integer(1));
+		System.out.println("Test Completed: Inserted Data in database.");
+	}
+	@Test
+	public void testGetAllProcessedOrder() throws Exception{
+		Integer user_id = 1;
+		orderDAO  = new OrderDAO();
+		List<Order> newOrder = orderDAO.getAllProcessedOrderFromUser(user_id);
+		for(Order ord : newOrder){
+			assertEquals(ord.getUser_id(),user_id);
+			orderDAO.deleteOpenOrderInDatabase(ord.getOrder_id());
+		}
+		
+		
+	}
 	
-	assertEquals(deletedOrder, isDeleted);
-	System.out.println("Test Completed: Deleted Data in database.");
-}
-
+	@Test
+	public void testGetAllSoldOrders() throws Exception{
+		System.out.println("start testGetAllSoldOrders");
+		orderDAO = new OrderDAO();
+		List <Order> soldList = orderDAO.getAllSoldOrderInDatabase();
+		
+		for(Order ord : soldList){
+			assertEquals(ord.getStatus(), "completed");
+			System.out.println(ord.getOrder_id());
+		}
+		
+	}
+	
+	
 }
