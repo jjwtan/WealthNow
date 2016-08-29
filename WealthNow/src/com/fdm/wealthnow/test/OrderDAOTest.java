7ad3adee7c2e497f173f5a73e4475e87804dd30d
Ray.Huang <Ray.Huang@LAPBTN11968.fdmgroup.local>
2016-08-29 11:16:01 +0800

    Ray: Updated OrderDAO with deleted test

--- WealthNow/src/com/fdm/wealthnow/test/OrderDAOTest.java
+++ WealthNow/src/com/fdm/wealthnow/test/OrderDAOTest.java
@@ -18,27 +18,32 @@ public void setup(){
 	OrderDAO.setConnectionType(ConnectionType.LOCAL_CONNECTION);
 }
 
+//@Test 
+//public void testCreatedDataInDatabase() throws Exception{
+//
+//orderDAO = new OrderDAO();
+//orderDAO.createOpenOrderInDatabase(new Integer(1), new Integer(100), "SGD", "B", new Integer(100), "AAPL", "M", "11 Sep 2011", new Double(88.9), "GC", "Open");
+//orderDAO.createOpenOrderInDatabase(new Integer(2), new Integer(101), "SGD", "B", new Integer(100), "GOOG", "LT", "05/05/2016", new Double(76.2), "GC", "Open");
+//orderDAO.createOpenOrderInDatabase(new Integer(3), new Integer(102), "SGD", "B", new Integer(100), "AMZN", "M", "06/05/2016", new Double(90.1), "GD", "Open");
+//
+//List<Order> TestList = orderDAO.getListOfOpenOrder(3);
+//Integer testcount = TestList.size();
+//
+//assertEquals(testcount, new Integer(3));
+//
+//
+//	System.out.println("Test Completed: Created Data in database.");
+//}
+
+
 @Test 
-public void testCreatedDataInDatabase() throws Exception{
-
-orderDAO = new OrderDAO();
-orderDAO.createOpenOrderInDatabase(new Integer(1), new Integer(100), "SGD", "B", new Integer(100), "AAPL", "M", "11 Sep 2011", new Double(88.9), "GC", "Open");
-orderDAO.createOpenOrderInDatabase(new Integer(2), new Integer(101), "SGD", "B", new Integer(100), "GOOG", "LT", "05/05/2016", new Double(76.2), "GC", "Open");
-orderDAO.createOpenOrderInDatabase(new Integer(3), new Integer(102), "SGD", "B", new Integer(100), "AMZN", "M", "06/05/2016", new Double(90.1), "GD", "Open");
-
-List<Order> TestList = orderDAO.getListOfOpenOrder(3);
-Integer testcount = TestList.size();
-
-assertEquals(testcount, new Integer(3));
-
-
-	System.out.println("Test Completed: Created Data in database.");
-}
-
-
-@Test 
-public void testDeleteDataInOpenOrder(){
-	System.out.println("Test Completed: Delete Data in database.");
+public void testDeleteDataInOpenOrder() throws Exception{
+	orderDAO = new OrderDAO();
+	boolean deletedOrder = orderDAO.deleteOpenOrderInDatabase(80);
+	boolean isDeleted = true;
+	
+	assertEquals(deletedOrder, isDeleted);
+	System.out.println("Test Completed: Deleted Data in database.");
 }
 
 }
