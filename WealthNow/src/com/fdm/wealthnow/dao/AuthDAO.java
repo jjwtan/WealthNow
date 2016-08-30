19e41151716f34fe999a628d68192582b011041c
Jeremy.Tan <Jeremy.Tan@LAPBTN12791.fdmgroup.local>
2016-08-30 13:54:29 +0800

    implemented fail login counter and increment of fail login counter

--- WealthNow/src/com/fdm/wealthnow/dao/AuthDAO.java
+++ WealthNow/src/com/fdm/wealthnow/dao/AuthDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 import com.fdm.wealthnow.common.UserAuth;
 import com.fdm.wealthnow.util.DBUtil;
@@ -14,7 +15,15 @@ public class AuthDAO extends DBUtil{
 	public static UserAuth authenticate(String username, String password) throws Exception {
 		Connection connect = getConnection();
 		
-		checkPassword(connect, username, password);
+		int failCount = getFailedCount(connect, username);
+		if (failCount == -1 ) {
+			System.out.println("username does not exist");
+		} else if(failCount <= 5 ) {
+			checkPassword(connect, username, password);
+		} else {
+			System.out.println("max attempt reached");
+		}
+		
 		
 		
 		connect.close();
@@ -32,20 +41,46 @@ public class AuthDAO extends DBUtil{
 		return rs.next();
 	}
 	
-	private void checkFailedCount() {
+	public static int getFailedCount(Connection connect, String username) throws SQLException {
 		
+		PreparedStatement ps;
+		ResultSet rs;
+		ps = connect.prepareStatement("Select fail_login_count from " +  USER_TABLE + " where user_name = ?");
+		ps.setString(1, username);
+		rs = ps.executeQuery();
+		if(rs.next()) {
+			return rs.getInt("fail_login_count");
+		} else {
+			return -1;
+		}
 	}
 	
-	private void incrementFailCount() {
-		
+	public static void incrementFailCount(Connection connect, String username) throws SQLException {
+		PreparedStatement ps;
+
+		ps = connect.prepareStatement("Update " + USER_TABLE + " Set fail_login_count = fail_login_count + 1 where user_name = ?" );
+		ps.setString(1, username);
+		ps.executeUpdate();
 	}
 	
-	private void getLastFailedTime() {
-		
+	public static Date getLastFailedTime(Connection connect, String username) throws SQLException {
+		PreparedStatement ps;
+		ResultSet rs;
+		ps = connect.prepareStatement("Select last_failedlogin from " +  USER_TABLE + " where user_name = ?");
+		ps.setString(1, username);
+		rs = ps.executeQuery();
+		if(rs.next()) {
+			return rs.getDate("last_failedlogin");
+		}
+		return null;
 	}
 	
-	private void resetLastFailedCountAndTime() {
+	public static void resetLastFailedCountAndTime(Connection connect, String username) throws SQLException {
+		PreparedStatement ps;
 		
+		ps = connect.prepareStatement("Update " + USER_TABLE + " Set fail_login_count = 0 where user_name = ?" );
+		ps.setString(1, username);
+		ps.executeUpdate();
 	}
 	
 	
