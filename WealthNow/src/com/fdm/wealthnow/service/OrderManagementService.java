4e1e99b33b10715627ca75717054ab61a17f2bdf
Saifullah.R <Saifullah.R@LAPBTN11975.fdmgroup.local>
2016-08-30 10:09:15 +0800

    Added Order Management Service version 1.0 final skeleton.

--- WealthNow/src/com/fdm/wealthnow/service/OrderManagementService.java
+++ WealthNow/src/com/fdm/wealthnow/service/OrderManagementService.java
@@ -2,12 +2,24 @@ package com.fdm.wealthnow.service;
 
 import java.util.List;
 
+import com.fdm.wealthnow.dao.OrderDAO;
+
 public class OrderManagementService {
 	/*
-	 * user submitting order
+	 * user submits order and the service will pass the details to the DAO for
+	 * creation.
 	 */
-	public void submitOrder() {
+	public void createOpenOrder() {
+		OrderDAO ord = new OrderDAO();
 
+	}
+
+	public void processOrder() {
+
+	}
+
+	public boolean validateOrderData() {
+		return true;
 	}
 
 	public void createStockHoldings() {
@@ -18,15 +30,20 @@ public class OrderManagementService {
 
 	}
 
+	public void deleteStockHoldings() {
+
+	}
+
 	/*
-	 * system check to see if trade is executable
+	 * system check to see if trade is executable - to be done at
+	 * OrderProcessing
 	 */
 	public boolean checkLimitofOrder() {
 		return false;
 	}
 
 	/*
-	 * getting price from stock service(Jeremy)
+	 * getting price from stock service(Jeremy) - to be done at OrderProcessing
 	 */
 	public float getPriceFromStockExchange() {
 		return (Float) null;
