c1f44e172977956a720cc6c01b233b0261422085
jjwtan <jjwtan@gmail.com>
2016-08-30 21:42:25 +0800

    addded authentication filter

---
+++ WealthNow/src/com/fdm/wealthnow/filter/AuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.fdm.wealthnow.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationFilter implements Filter {
+
+	private ServletContext context;
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.context = fConfig.getServletContext();
+		this.context.log("AuthenticationFilter initialized");
+	}
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		String uri = req.getRequestURI();
+		System.out.println("Requested Resource:"+uri);
+		
+		// Special handle request to login page.Otherwise we will end up in infinite loop!
+		if (!uri.endsWith("/login.jsp") && !uri.endsWith("/LoginServlet")) {
+			//Do not create a new session if it does not exist!
+			HttpSession session = req.getSession(false);
+			if(session == null || session.getAttribute("loggedInUser") == null) {
+				System.out.println("Unauthorized access request, redirecting to login page...");
+				res.sendRedirect("login.jsp");
+				return;
+			}
+		}
+
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	public void destroy() {
+		//close any resources here
+	}
+
+}
