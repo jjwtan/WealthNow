a2dee49d0554c89b8743da6c832486c6dd87771f
Jeremy.Tan <Jeremy.Tan@LAPBTN12791.fdmgroup.local>
2016-08-31 15:27:40 +0800

    add username duplicate validation check

--- WealthNow/src/com/fdm/wealthnow/service/UserRegisterService.java
+++ WealthNow/src/com/fdm/wealthnow/service/UserRegisterService.java
@@ -1,14 +1,31 @@
 package com.fdm.wealthnow.service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 import com.fdm.wealthnow.common.User;
+import com.fdm.wealthnow.util.DBUtil;
 
 @SuppressWarnings("unused")
-public class UserRegisterService {
+public class UserRegisterService extends DBUtil {
 	
 	User user;
+	Connection connect;
+	
+	private static final String USER_TABLE = "user1";
+	
+	public UserRegisterService() {
+		try {
+			connect = getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 	public Integer registerUser(User user){
 		return 1;
@@ -22,8 +39,25 @@ public class UserRegisterService {
 		return true;
 	}
 	
+	/**
+	 * validate that only one of this username exist, i.e there are not same username
+	 * @param userName
+	 * @param user
+	 * @return
+	 */
 	private boolean validateUserName(String userName, User user){
-		return true;
+		try {
+			PreparedStatement ps = connect.prepareStatement("Select * from " + USER_TABLE + " where user_name = ?");
+			ps.setString(1, userName);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			return rs.next();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
 	}
 	
 	private User createUserObject(String userName, String firstName, 
