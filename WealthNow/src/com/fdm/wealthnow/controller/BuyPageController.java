f14c713a5569772958148e9ae8714d1e18cdba3a
Saifullah.R <Saifullah.R@LAPBTN11975.fdmgroup.local>
2016-08-31 17:00:47 +0800

    testing codes in BuyPageController

--- WealthNow/src/com/fdm/wealthnow/controller/BuyPageController.java
+++ WealthNow/src/com/fdm/wealthnow/controller/BuyPageController.java
@@ -1,11 +1,14 @@
 package com.fdm.wealthnow.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class BuyPageController
@@ -35,7 +38,22 @@ public class BuyPageController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		HttpSession session = request.getSession();
+		
 		doGet(request, response);
+		String ordertype = request.getParameter("ordertype");
+		String quantity  = request.getParameter("quantity");
+		String stock_symbol = request.getParameter("stock_symbol");
+		String price_type = request.getParameter("price_type");
+		String term = request.getParameter("term");
+		
+		//just for testing
+		PrintWriter pw = response.getWriter();
+		pw.println(ordertype+ " "+ quantity+ " "+ stock_symbol + " "+ price_type + " "+term);
+		System.out.println(ordertype+ " "+ quantity+ " "+ stock_symbol + " "+ price_type + " "+term);
+		
+		
 	}
 
 }
