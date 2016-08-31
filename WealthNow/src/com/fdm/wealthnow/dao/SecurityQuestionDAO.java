816e1b0d55867d81d9969b8dc6359b8f964c53f2
Jeremy.Tan <Jeremy.Tan@LAPBTN12791.fdmgroup.local>
2016-08-31 13:20:57 +0800

    added retrieving of questions and ans of userId, retriving all questions

--- WealthNow/src/com/fdm/wealthnow/dao/SecurityQuestionDAO.java
+++ WealthNow/src/com/fdm/wealthnow/dao/SecurityQuestionDAO.java
@@ -1,25 +1,102 @@
 package com.fdm.wealthnow.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.fdm.wealthnow.common.SecurityQnAndAns;
+import com.fdm.wealthnow.util.DBUtil;
 
-public class SecurityQuestionDAO {
-	public int getQuestionId() {
+
+public class SecurityQuestionDAO extends DBUtil{
+	private static final String USER_TABLE = "user1";
+	
+	public List<SecurityQnAndAns> getUserQnAndAns(Connection connect, int userId) {
+		List<SecurityQnAndAns> qnIdList = new ArrayList<>();
 		
-		return 1;
+		PreparedStatement ps = null;
+		try {
+			ps = connect.prepareStatement("Select * from SecurityQuestion where user_id = ?");
+			ps.setInt(1, userId);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				qnIdList.add(new SecurityQnAndAns(rs.getInt("question_id"), rs.getString("answer")));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return qnIdList;
 	}
 	
-	public String getQuestion(int questionId) {
-		
-		return "";
-	}
-	
-	public List<String> getAllQuestions() {
-		
+	public String getQuestion(Connection connect, int questionId) {
+		PreparedStatement ps = null;
+		try {
+			ps = connect.prepareStatement("Select * from questionbank where question_id = ?");
+			ps.setInt(1, questionId);
+			
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				return rs.getString("question");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 	
-	public int addSecurityAnswer(int userId,int questionId, String ans) {
+	public List<String> getAllQuestions(Connection connect) {
+		ArrayList<String> qnList = new ArrayList<>();
+		PreparedStatement ps = null;
+		
+		try {
+
+			ps = connect.prepareStatement("Select * from QuestionBank");
+
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				qnList.add(rs.getString("question"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return qnList;
+	}
+	
+	public int addSecurityAnswer(Connection connect, int userId, SecurityQnAndAns response) {
+		PreparedStatement ps = null;
+		try {
+			int nextId = getSequenceID("security_id_seq");
+			ps = connect.prepareStatement("INSERT INTO SecurityQuestion (security_id, user_id, answer , question_id ) VALUES (" + nextId + " , ?, ?, ?)");
+			ps.setInt(1, userId);
+			ps.setString(2, response.getAns());
+			ps.setInt(3, response.getQnId());
+			
+			ps.executeUpdate();
+
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return 1;
 	}
 	
