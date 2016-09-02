a7467e722eee371596169b39bc0ff8940d21f642
jjwtan <jjwtan@gmail.com>
2016-09-02 10:43:31 +0800

    added and tested debit/credit of balance in DAO

--- WealthNow/src/com/fdm/wealthnow/dao/UserAccountDAO.java
+++ WealthNow/src/com/fdm/wealthnow/dao/UserAccountDAO.java
@@ -9,12 +9,12 @@ import com.fdm.wealthnow.common.UserAccount;
 import com.fdm.wealthnow.util.DBUtil;
 
 public class UserAccountDAO extends DBUtil{
-	private static final String USER_TABLE = "useraccount";
+	private static final String USER_ACCOUNT_TABLE = "useraccount";
 
 	
 	public UserAccount getAccountBalance(Connection connect, int userId) {
 		try {
-			PreparedStatement ps = connect.prepareStatement("select * from " + USER_TABLE + " where user_id = ?");
+			PreparedStatement ps = connect.prepareStatement("select * from " + USER_ACCOUNT_TABLE + " where user_id = ?");
 			ps.setInt(1, userId);
 			
 			ResultSet rs = ps.executeQuery();
@@ -28,6 +28,57 @@ public class UserAccountDAO extends DBUtil{
 		}
 		
 		return null;
+	}
+	
+	public double debitBalance(Connection connect, int userId, double amountToDeduct) {
+		if (amountToDeduct < 0) {
+			System.out.println("invalid parameter amount");
+			return -1;
+		}
+		try {
+			PreparedStatement ps = connect.prepareStatement("Update " + USER_ACCOUNT_TABLE + " SET balance = balance - ? where user_id = ?");
+			ps.setDouble(1, amountToDeduct);
+			ps.setInt(2, userId);
+			
+			int result = ps.executeUpdate();
+			
+			if (result == 0) {
+				System.out.println("user id does not exist");
+				return -1;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return getAccountBalance(connect, userId).getBalance();
+	}
+	
+	public double creditBalance(Connection connect, int userId, double amountToCredit) {
+		
+		if (amountToCredit < 0) {
+			System.out.println("invalid parameter amount");
+			return -1;
+		}
+		try {
+			PreparedStatement ps = connect.prepareStatement("Update " + USER_ACCOUNT_TABLE + " SET balance = balance + ? where user_id = ?");
+			ps.setDouble(1, amountToCredit);
+			ps.setInt(2, userId);
+			
+			int result = ps.executeUpdate();
+			
+			if (result == 0) {
+				System.out.println("user id does not exist");
+				return -1;
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return getAccountBalance(connect, userId).getBalance();
 	}
 
 }
