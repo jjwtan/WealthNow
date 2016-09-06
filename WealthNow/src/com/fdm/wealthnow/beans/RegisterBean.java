9898723c7b670a561c204e9853b80c6e4d763708
Jeremy.Tan <Jeremy.Tan@LAPBTN12791.fdmgroup.local>
2016-09-06 12:33:09 +0800

    added registrationbean for user profile fields

---
+++ WealthNow/src/com/fdm/wealthnow/beans/RegisterBean.java
@@ -0,0 +1,74 @@
+package com.fdm.wealthnow.beans;
+
+import java.io.Serializable;
+
+public class RegisterBean implements Serializable {
+	String username;
+	String password;
+	String firstName;
+	String lastName;
+	String birthday;
+	String email;
+	String address;
+	String maidenName;
+	int phoneNumber;
+	
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getMaidenName() {
+		return maidenName;
+	}
+	public void setMaidenName(String maidenName) {
+		this.maidenName = maidenName;
+	}
+	
+	
+}
