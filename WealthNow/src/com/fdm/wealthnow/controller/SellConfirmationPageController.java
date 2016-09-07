762127ac976f01d464a276f9840d12ca4f918d1f
Haikal.Yahya <Haikalyaya14@gmail.com>
2016-09-07 10:00:08 +0800

    new name controller

--- WealthNow/src/com/fdm/wealthnow/controller/SellConfirmationPageController.java
+++ WealthNow/src/com/fdm/wealthnow/controller/SellConfirmationPageController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class SellConfirmationPage
  */
 @WebServlet("/SellConfirmationPage")
-public class SellConfirmationPage extends HttpServlet {
+public class SellConfirmationPageController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SellConfirmationPage() {
+    public SellConfirmationPageController() {
         super();
         // TODO Auto-generated constructor stub
     }
